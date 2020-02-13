5f90f8bb234da77d8610e8926e2e490ab92ad7de
robin harris <robjnz@outlook.com>
2020-02-13 09:51:01 +0000

    update

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,19 +1,42 @@
 <template>
   <Layout>
-<b-container class="container">
-  <b-row>
-   
-<h1>What on in East London February 2020</h1>
+    <b-container class="container">
+      <b-row>
+        <h1>What on in East London February 2020</h1>
 
-    <br>
- <b-embed type="iframe"
+        <br />
+        <b-embed
+          type   = "iframe"
           aspect = "16by9"
-          src    = "https://walls.io/snjfm?nobackground=1&amp;show_header=0" style = "border:0;height:800px;width:100%" title = "londoneast.life"
+          src    = "https://walls.io/snjfm?nobackground=1&amp;show_header=0"
+          style  = "border:0;height:800px;width:100%"
+          title  = "londoneast.life"
           allowfullscreen
-  ></b-embed>
-  </b-row>
-</b-container>
+        ></b-embed>
+      </b-row>
+      <div class="port container" id="works">
+        
+        <div class="row">
+          <div class="work col-md-6">
+            <div class="work-card">
+              <H4>Home Matches 15 February 2020</H4>
+              <p>
+                <g-image src="~/images/charlton.png" width="40" />
+                Charlton Athletic V Blackburn Rovers<br />
+                <g-image src="~/images/belvedere.jpg" width="40" />
+                Erith & Belvedere V Greenwich Borough<br />
+                <g-image src="~/images/hornchurch.jpg" width="40" />
+                Hornchurch V Merstham<br />
+                <g-image src="~/images/redbridge.jpg" width="40" />
+                Redbridge V Cockfosters<br />
+              </p>
 
+              <g-link to="/sport/football/">More Info</g-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </b-container>
   </Layout>
 </template>
 <script>
@@ -31,32 +54,26 @@ export default {
 };
 </script>
 
-
 <style>
-
-.Hero{
-padding-top   : 10rem;
-padding-bottom: 10rem;
-background    : darksalmon;
-color         : #337;
-  
+.Hero {
+  padding-top   : 10rem;
+  padding-bottom: 10rem;
+  background    : darksalmon;
+  color         : #337;
 }
-.Port{
-padding: 5rem;
-  
+.Port {
+  padding: 5rem;
 }
-.work{
-padding: 2rem 1rem;
-  
+.work {
+  padding: 2rem 1rem;
 }
-.work-card{
-  box-shadow   : 0px 0px 20px rgba(0,0,0.1);
-  padding      : 1rem ;
+.work-card {
+  box-shadow   : 0px 0px 20px rgba(0, 0, 0.1);
+  padding      : 1rem;
   border-radius: 0.2rem;
+  color        : #00ff7f;
 }
 .home-links a {
   margin-right: 1rem;
 }
-
-
 </style>
