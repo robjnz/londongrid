05549d91d38e4fc867e5e1ab1407daebd53ce89f
robin harris <robjnz@outlook.com>
2020-03-13 11:51:51 +0000

    update

--- src/components/Nightclubs/Faces.vue
+++ src/components/Nightclubs/Faces.vue
@@ -8,7 +8,7 @@
   <h3 class="tory">Faces Ilford</h3>
     <a href="http://bit.ly/2uASyos" target="_blank"
       ><b-img
-        src = "https://res.cloudinary.com/dzhbfdfa5/image/upload/c_scale,h_200,w_300/v1549703857/faces_zpm8ou.jpg"
+        src = "https://res.cloudinary.com/dzhbfdfa5/image/upload/c_scale,h_200,w_300/v1583781635/faces_vyd7iu.jpg"
         fluid-grow
         alt = "Faces Ilford"
     /></a>
@@ -20,8 +20,8 @@
         IG2 6LE
       </g-link>
     </h6>
-    <p class="football">Closed<br /><br /></p>
-    <g-link class="labour" to="http://bit.ly/2uASyos" target="_blank">
+    <p class="football">Relaunching Soon<br /><br /></p>
+    <g-link class="labour" to="http://bit.ly/2wL0pAh" target="_blank">
       More Info
     </g-link>
   </b-card>
